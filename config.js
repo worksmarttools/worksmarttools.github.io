0f3f930ef5715385ede1013c8eb39f0de9f6d982
worksmarttools <towfik.passive@gmail.com>
2026-04-09 01:01:58 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// IMPORTANT: Replace with your Google Apps Script Web App URL
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbyVYqNB1vhMenD7k5OchYtdN2eQSzbrmkAJuB8286OKiZbG4Vnram97Oyr9j43evffs/exec';
