6eff11f57f4d47aedab8ba7c25cc86175467cada
worksmarttools <towfik.passive@gmail.com>
2026-04-09 01:29:58 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // IMPORTANT: Replace with your Google Apps Script Web App URL
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxbjUk6At79FPqnJNyujTGcBJt1wpTBTfi4bWS1U8_bK19Tj8Ebc7iKYz9Jf5XmQv9lkQ/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwdt5Gy3D176yE-mfgGgPUwCJgA6jEMemP_T0F-78cBMxp93GiZRfWvZEH36PreunhGvw/exec';
