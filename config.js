fea89197337a5ebc4cbc11e920bc510fbcec45f2
worksmarttools <towfik.passive@gmail.com>
2026-04-10 12:47:32 +0600

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// IMPORTANT: Replace with your Google Apps Script Web App URL
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzD6Ha_BDEem25YKujcKyo6d74Y07lM79P1V6OC1YsYAcRXIqF_BVSbFHuNV2asRdNkvw/exec';
